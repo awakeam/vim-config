41668f09d3249afdc4b58e8494de9d3d6d20be03
awakeam <1475075514@qq.com>
2021-01-12 14:56:23 +0800

    update

---
+++ apt-install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+#================
+# deb source
+#================
+
+# deb http://mirrors.aliyun.com/debian buster main contrib non-free
+# deb-src http://mirrors.aliyun.com/debian buster main contrib non-free
+
+# deb http://security.debian.org/debian-security buster/updates main contrib
+# deb-src http://security.debian.org/debian-security buster/updates main contrib
+
+# deb http://apt.llvm.org/buster/ llvm-toolchain-buster-11 main
+# deb [arch=amd64] https://download.docker.com/linux/debian buster stable
+# deb https://typora.io/linux ./
+
+
+#================
+# apt install
+#================
+
+# sudo apt install i3 rofi feh flameshot fcitx fonts-firacode zathura terminator fzf mindmaster rg rdesktop irssi
+
+# oh-my-zsh
+# https://github.com/ohmyzsh/ohmyzsh/blob/master/tools/install.sh
+
+
+
+#================
+# apt 安装记录
+#================
+
+# /var/log/dpkg.log
+
+
